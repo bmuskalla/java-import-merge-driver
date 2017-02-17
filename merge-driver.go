2344a57e6771ec39d90142eeec5b6349442a3bdf
Benjamin Muskalla <benjamin.muskalla@tasktop.com>
2017-02-17 01:18:18 +0100

    Stub for merge driver

---
+++ merge-driver.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	// driver = foo %O %A %B %L %P
+	// ancestor’s version (%O)
+	// current version (%A) <<<<<<
+	// and the other branches' version (%B).
+	panic("tbd")
+}
